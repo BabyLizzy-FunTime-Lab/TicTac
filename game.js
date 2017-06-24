11cddd4153155ad4cd9332decd6c844827f26314
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2017-06-24 17:02:25 +0200

    row and columns broke

--- game.js
+++ game.js
@@ -1,65 +1,57 @@
 let gameField = [
-	["X", "O", "O"],
-	["X", "O", "X"],
-	["X", null, null]
+	["O", "O", "O"],
+	["X", "X", "O"],
+	["O", null, "O"]
 ]
 
-// for (var row = 0; row < gameField.length; row++) {
-// 	// Tip from G: rowSpot can be also be called collum index because you are
-// 	// now checking within the collums.
-// 	let rowMatch = true;
-// 	for (var rowSpot = 1; rowSpot < gameField[row].length; rowSpot++) {
-// 		// You can select a row by going back to the gameField[row].
+for (var row = 0; row < gameField.length; row++) {
+	// Tip from G: rowSpot can be also be called collum index because you are
+	// now checking within the collums.
+	let rowMatch = true;
+	for (var rowSpot = 1; rowSpot < gameField[row].length; rowSpot++) {
+		// You can select a row by going back to the gameField[row].
 		
-// 		// winningCharacter stores the previous character position so it can
-// 		// be compaired to the current one.  
-// 		let winningCharRow = gameField[row][rowSpot - 1];
+		// winningCharacter stores the previous character position so it can
+		// be compaired to the current one.  
+		let winningCharRow = gameField[row][rowSpot - 1];
 
-// 		// This checks the rows.
-// 		// if the characters are the same but also null it will break.
-// 		if(winningCharRow !== gameField[row][rowSpot] || gameField[row][rowSpot] === null) {
-// 			console.log("no winner in row: " + row);
-// 			rowMatch = false;
-// 			break;
-// 		} 
+		// This checks the rows.
+		// if the characters are the same but also null it will break.
+		if(winningCharRow !== gameField[row][rowSpot] || gameField[row][rowSpot] === null) {
+			console.log("no winner in row: " + row);
+			rowMatch = false;
+			break;
+		} 
 
-// 		if(rowMatch === true && gameField[row][0] !== null) {
-// 			console.log("Winner is: " + gameField[row][0]);
-// 			break;
-// 			// is it logicaly ok for me to but a break here?
-// 			// Here by I assume that the winner is gameField[row][0];
-// 		}			
-// 	}
-// }
+		if(rowMatch === true) {
+			console.log("Winner is: " + gameField[row][0]);
+			break;
+		}			
+	}
+}
 
+
+// This will go over the columns
+// Row number is set to start looping from the next row.
 for (var row = 1; row < gameField.length; row++) {
+	// Then we go over the columns
 	let columnMatch = true;
-
 	for (var columnSpot = 0; columnSpot < gameField[row].length; columnSpot++) {
-	 	if (gameField[row - 1][columnSpot] !== gameField[row][columnSpot]) {
+
+		let winningCharCol = gameField[row - 1][columnSpot];
+
+		// Here the current spot is comparaid with the previous spot in the columns.
+	 	if (winningCharCol !== gameField[row][columnSpot] || gameField[row][columnSpot] === null) {
 	 		console.log("no winner in column: " + columnSpot);
 	 		columnMatch = false;
 	 		break;
 	 	}
 
-	 	if (columnMatch === true && )
-	 	
-	 } 
-	
-
-	// This checks the colums.
-		// if(gameField[row][rowSpot - 1] !== gameField[row + 1][rowSpot - 1]) {
-		// 	console.log("no winner in colum: " + (rowSpot -1));
-		// 	break;
-		// } else {
-		// 	console.log("no winner in colum: " + (rowSpot -1));
-		// 	break;
-		// }
-
-		// 	if(gameField[row][0] !== null) {
-		// 		console.log(gameField[row][0] + " won!!");				
-		// 	}
-		// } 
+	 	if (columnMatch === true) {
+			console.log("Winner is: " + winningCharCol);
+			break;
+		}
+	} 
 }
 
 // for (var i = 0; i < Things.length; i++) {
