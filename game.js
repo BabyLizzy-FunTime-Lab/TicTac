0711fdf8ffb31053c5c9e946de1aff92ee0a8be2
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2017-06-25 09:26:41 +0200

    logicTrueFalse does not work. Too much work for no gain.

--- game.js
+++ game.js
@@ -1,58 +1,142 @@
 let gameField = [
-	["O", "O", "O"],
 	["X", "X", "O"],
-	["O", null, "O"]
+	["O", "X", "X"],
+	["X", "O", "X"]
 ]
 
 for (var row = 0; row < gameField.length; row++) {
 	// Tip from G: rowSpot can be also be called collum index because you are
 	// now checking within the collums.
-	let rowMatch = true;
-	for (var rowSpot = 1; rowSpot < gameField[row].length; rowSpot++) {
-		// You can select a row by going back to the gameField[row].
-		
-		// winningCharacter stores the previous character position so it can
-		// be compaired to the current one.  
-		let winningCharRow = gameField[row][rowSpot - 1];
+	let rowMatch = true; 
 
-		// This checks the rows.
-		// if the characters are the same but also null it will break.
-		if(winningCharRow !== gameField[row][rowSpot] || gameField[row][rowSpot] === null) {
+	for (var rowSpot = 1; rowSpot < gameField[row].length; rowSpot++) {
+
+		let	previousRowPosition = gameField[row][rowSpot - 1];
+		let nextRowPosition = gameField[row][rowSpot + 1];
+		
+		if(previousRowPosition !== gameField[row][rowSpot] ||
+			gameField[row][rowSpot] !== nextRowPosition) {
 			console.log("no winner in row: " + row);
 			rowMatch = false;
 			break;
 		} 
 
-		if(rowMatch === true) {
-			console.log("Winner is: " + gameField[row][0]);
+		//  I don't understand why it prints no winner when I have 3 null.
+		
+		if(rowMatch === true && previousRowPosition !== null) {
+			console.log("Row Winner is: " + gameField[row][rowSpot]);
 			break;
 		}			
 	}
 }
 
+ ////DOES NOT WORK!!!!!!//////
+for (var row = 1; row < gameField.length; row++) {
+	// Tip from G: rowSpot can be also be called collum index because you are
+	// now checking within the collums.
+	let colMatch = true; 
+
+	for (var col = 0; col < gameField[row].length; col++) {
+
+		let	previousColPosition = gameField[1 - 1][col];
+		let nextColPosition = gameField[1 + 1][col];
+		
+		if(previousColPosition !== gameField[1][col] ||
+			gameField[1][col] !== nextColPosition) {
+			console.log("no winner in column: " + col);
+			colMatch = false;
+			break;
+		} 
+
+		
+		if(colMatch === true && previousColPosition !== null) {
+			console.log("Col Winner is: " + gameField[row][col]);
+			break;
+		}			
+	}
+}
+
+for (var row = 1; row < gameField.length; row++) {
+	// Tip from G: rowSpot can be also be called collum index because you are
+	// now checking within the collums.
+	let rowMatch = true; 
+
+	for (var col = 1; col < gameField[row].length; col++) {
+
+		let	previousColPosition = gameField[row - 1][col - 1];
+		let nextColPosition = gameField[row + 1][col + 1];
+		
+		if(previousColPosition !== gameField[row][col] ||
+			gameField[row][col] !== nextColPosition) {
+			console.log("no winner in column: " + col);
+			rowMatch = false;
+			break;
+		} 
+
+		//  I don't understand why it prints no winner when I have 3 null.
+		
+		if(rowMatch === true && previousColPosition !== null) {
+			console.log("Col Winner is: " + gameField[row][col]);
+			break;
+		}			
+	}
+}
+
+// for (var row = 0; row < gameField.length; row++) {
+// 	// Tip from G: rowSpot can be also be called collum index because you are
+// 	// now checking within the collums.
+// 	let rowMatch = null; 
+
+// 	for (var rowSpot = 1; rowSpot < gameField[row].length; rowSpot++) {
+
+// 		let	previousRowPosition = gameField[row][rowSpot - 1];
+// 		let nextRowPosition = gameField[row][rowSpot + 1];
+		
+// 		if(previousRowPosition === gameField[row][rowSpot]) {
+
+// 			if(gameField[row][rowSpot] === nextRowPosition) {
+// 				console.log("Winner is: " + gameField[row][0]);
+// 				break;
+// 			}
+
+// 			console.log("no winner in row: " + row);
+// 			rowMatch = false;
+// 			break;
+// 		} 
+
+// 		// && previousRowPosition !== null
+// 		if(rowMatch === true ) {
+			
+// 		}			
+// 	}
+// }
+
+
+
+
 
 // This will go over the columns
 // Row number is set to start looping from the next row.
-for (var row = 1; row < gameField.length; row++) {
-	// Then we go over the columns
-	let columnMatch = true;
-	for (var columnSpot = 0; columnSpot < gameField[row].length; columnSpot++) {
+// for (var row = 1; row < gameField.length; row++) {
+// 	// Then we go over the columns
+// 	let columnMatch = true;
+// 	for (var col = 0; col < gameField[row].length; col++) {
 
-		let winningCharCol = gameField[row - 1][columnSpot];
+// 		let winningCharCol = gameField[row - 1][col];
 
-		// Here the current spot is comparaid with the previous spot in the columns.
-	 	if (winningCharCol !== gameField[row][columnSpot] || gameField[row][columnSpot] === null) {
-	 		console.log("no winner in column: " + columnSpot);
-	 		columnMatch = false;
-	 		break;
-	 	}
+// 		// Here the current spot is comparaid with the previous spot in the columns.
+// 	 	if (winningCharCol !== gameField[row][col] || gameField[row][col] === null) {
+// 	 		console.log("no winner in column: " + col);
+// 	 		columnMatch = false;
+// 	 		break;
+// 	 	}
 
-	 	if (columnMatch === true) {
-			console.log("Winner is: " + winningCharCol);
-			break;
-		}
-	} 
-}
+// 	 	if (columnMatch === true) {
+// 			console.log("Winner is: " + winningCharCol);
+// 			break;
+// 		}
+// 	} 
+// }
 
 // for (var i = 0; i < Things.length; i++) {
 // 	Things[i]
